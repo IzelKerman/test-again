4fc1d09ee537d8e10b24d78300cd8b45fe1148cd
Nathan Vanbeneden <58216235+IzelKerman@users.noreply.github.com>
2022-03-20 09:52:56 +0100

    Add files via upload

---
+++ compute.py
@@ -0,0 +1,199 @@
+import numpy as np
+import scipy.integrate as sc
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from PIL import Image
+
+
+def ell_to_cart(r, theta, phi, a=1):
+    rho = np.sqrt(r ** 2 + a ** 2)
+    return rho * np.sin(theta) * np.cos(phi), rho * np.sin(theta) * np.sin(phi), r * np.cos(theta)
+
+
+def cot(x):
+    return 1 / np.tan(x)
+
+
+def g(r, theta, a):
+    rho = np.sqrt(r ** 2 + (a * np.cos(theta)) ** 2)
+    Delta = r ** 2 - 2 * r + a ** 2
+    Sigma = np.sqrt((r ** 2 + a ** 2) ** 2 - a ** 2 * Delta * np.sin(theta) ** 2)
+    alpha = rho * np.sqrt(Delta) / Sigma
+    omega = 2 * a * r / Sigma ** 2
+    Omega = Sigma * np.sin(theta) / rho
+    g_00 = (Omega * omega) ** 2 - alpha ** 2
+    g_11 = rho ** 2 / Delta
+    g_22 = rho ** 2
+    g_33 = Omega ** 2
+    g_03 = - Omega ** 2 * omega
+    return [g_00, g_11, g_22, g_33, g_03]
+
+
+def g_inv(r, theta):
+    pass
+
+
+def scalar_prod(r, theta, u, v, G=None):
+    if G is None:
+        g_ = g(r, theta)
+    else:
+        g_ = G
+    return g_[0] * u[0] * v[0] + g_[1] * u[1] * u[1] + g_[2] * u[2] * v[2] + g_[3] * u[3] * v[3] + g_[4] * (u[0] * v[3] + u[3] * v[0])
+
+
+def flat(r, theta, u, G=None):
+    if G is None:
+        g_ = g(r, theta)
+    else:
+        g_ = G
+    return np.array([g_[0] * u[0] + g_[4] * u[3], g_[1] * u[1], g_[2] * u[2], g_[4] * u[0] + g_[3] * u[3]])
+
+
+def sharp(r, theta, u):
+    pass
+
+
+def f_kerr(t, x):
+    #x = r, theta, phi, p_r, p_theta, a, b, q
+    r, theta, phi, p_r, p_theta, a, b, q = x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7]
+    rho = np.sqrt(r ** 2 + (a * np.cos(theta)) ** 2)
+    Delta = r ** 2 - 2 * r + a ** 2
+    P = r ** 2 + a ** 2 - a * b
+    R = P ** 2 - Delta * ((b - a) ** 2 + q)
+    Theta = q - (b * cot(theta)) ** 2 + (a * np.cos(theta)) ** 2
+    drho_r = r / rho
+    drho_theta = - a ** 2 * np.cos(theta) * np.sin(theta) / rho
+    dDelta_r = 2 * (r - 1)
+    dR_r = 4 * r * P - ((b - a) ** 2 + q) * dDelta_r
+    dTheta_theta = 2 * cot(theta) * (1 + cot(theta) ** 2) * b ** 2 - 2 * np.cos(theta) * np.sin(theta) * a ** 2
+
+    dr = Delta * p_r / rho ** 2
+    dtheta = p_theta / rho ** 2
+    #dphi = (a * P / Delta + (b - a) + b * cot(theta) ** 2) / rho ** 2
+    dphi = ((a - 2 * b) * P / Delta + b * (1 + cot(theta) ** 2) - a) / rho ** 2
+    truc = Delta * p_r ** 2 + p_theta ** 2 - R / Delta - Theta
+    dp_r = (truc * drho_r / rho - (p_r ** 2 + R / Delta ** 2) * dDelta_r / 2 + dR_r / (2 * Delta)) / rho ** 2
+    dp_theta = (truc * drho_theta / rho + dTheta_theta / 2) / rho ** 2
+    """if 0 <= t % 100 <= 0.002:
+        print(int(t) / 1000)"""
+
+    return np.array([dr, dtheta, dphi, dp_r, dp_theta, 0, 0, 0])
+
+
+def f_kerr_backward(t, x):
+    f = f_kerr(t, x)
+    return np.array([-f[0], -f[1], -f[2], -f[3], -f[4], f[5], -f[6], f[7]])
+
+
+class System:
+    def __init__(self, a, x, fov, im_size, im_name, background):
+        self.a = a
+        self.x = x
+        self.delta_x = fov[0] / 2
+        self.delta_y = fov[1] / 2
+        self.N_x = im_size[0]
+        self.N_y = im_size[1]
+        self.L_x = np.tan(self.delta_x)
+        self.L_y = np.tan(self.delta_y)
+        self.dx = 2 * self.L_x / self.N_x
+        self.dy = 2 * self.L_y / self.N_y
+        image_background = Image.open(background)
+        self.back_pixels = image_background.load()
+        self.background_size = image_background.size
+
+    def create_image(self, image_name):
+        img = Image.new('RGB', (self.N_x, self.N_y), (0, 0, 0))
+        img.save(image_name)
+
+    def compute_angle(self, i, j):
+        n = np.array([1, self.L_x * (-1 + (2 * i + 1) / self.N_x), self.L_y * (-1 + (2 * j + 1) / self.N_y)])
+        n = n / np.linalg.norm(n)   #composante de la direction spatiale (au signe près) dans la base ON e_r,...
+        """theta = np.arctan2(np.sqrt(n[0] ** 2 + n[1] ** 2), -n[2])
+        phi = np.arctan2(n[1], n[0])
+        if phi > np.pi:
+            phi = phi - 2 * np.pi
+        I = np.floor((phi + np.pi) * self.background_size[0] / (2 * np.pi))
+        J = np.floor(theta * self.background_size[1] / np.pi)
+        return self.back_pixels[I, J]"""
+        G = g(self.x[0], self.x[1], self.a)
+        #Gamma = - np.sqrt(G[3]) * n[2] * (G[0] + 1) / (2 * G[4])
+        #Gamma = (- G[4] * n[2] + np.sqrt(G[4] ** 2 * n[2] ** 2 - G[0] * G[3])) / np.sqrt(G[3])
+        #p = np.array([1, Gamma * n[0], - Gamma * n[2], Gamma * n[1]])
+        p = np.array([1, n[0] / np.sqrt(G[1]), -n[2] / np.sqrt(G[2]), n[1] / np.sqrt(G[3]) - G[4] / G[3]])
+        p = flat(self.x[0], self.x[1], p, G)
+        p = - p / p[0]
+        b = p[3]
+        q = p[2] ** 2 + cot(self.x[1]) ** 2 * b ** 2 - self.a ** 2 * np.cos(self.x[1]) ** 2
+        sol = sc.solve_ivp(f_kerr, [0, -1000], [self.x[0], self.x[1], self.x[2], p[1], p[2], self.a, b, q])
+        """fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        X = [ell_to_cart(sol.y[0][i], sol.y[1][i], sol.y[2][i], self.a) for i, y_i in enumerate(sol.y[0])]
+        x = [x_i[0] for x_i in X]
+        y = [x_i[1] for x_i in X]
+        z = [x_i[2] for x_i in X]
+        ax.plot(x, y, z)
+        plt.show()"""
+
+        if sol.y[0][-1] <= 1000:
+            return (0, 0, 0)
+        else:
+            # on est loin donc ellipsoid approx cartésien
+            #theta = sol.y[1][-1] % (np.pi)  # ici c'est faux
+            if sol.y[1][-1] % (2 * np.pi) <= np.pi:
+                theta = sol.y[1][-1] % np.pi
+                phi = sol.y[2][-1] % (2 * np.pi)
+            else:
+                theta = (2 * np.pi - sol.y[1][-1]) % np.pi
+                phi = (sol.y[2][-1] + np.pi) % (2 * np.pi)
+            phi = sol.y[2][-1] % (2 * np.pi)
+            if phi > np.pi:
+                phi = phi - 2 * np.pi
+            I = np.floor((phi + np.pi) * self.background_size[0] / (2 * np.pi))
+            J = np.floor(theta * self.background_size[1] / np.pi)
+            try:
+                return self.back_pixels[I, J]
+            except:
+                print("FUCK", I, J)
+
+
+if __name__ == "__main__":
+    a = 0.5
+    b = 0
+    q = 0
+    r = 10
+    theta = np.pi / 2
+    phi = 0
+    p_r = 0
+    p_theta = 0
+
+    rho = np.sqrt(r ** 2 + (a * np.cos(theta)) ** 2)
+    Delta = r ** 2 - 2 * r + a ** 2
+    Sigma = np.sqrt((r ** 2 + a ** 2) ** 2 - a ** 2 * Delta * np.sin(theta) ** 2)
+    alpha = rho * np.sqrt(Delta) / Sigma
+    omega = 2 * a * r / Sigma ** 2
+    Omega = Sigma * np.sin(theta) / rho
+
+    print(omega, alpha, Omega)
+    b = 1 / (omega + alpha / Omega)
+    #b = (r ** 2 + a ** 2 - a * b) / np.sqrt(Delta) + a
+
+    plt.style.use('dark_background')
+
+    sol = sc.solve_ivp(f_kerr, [0, 1125], [r, theta, phi, p_r, p_theta, a, b, q])
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    X = [ell_to_cart(sol.y[0][i], sol.y[1][i], sol.y[2][i], a) for i, y_i in enumerate(sol.y[0])]
+    x = [x_i[0] for x_i in X]
+    y = [x_i[1] for x_i in X]
+    z = [x_i[2] for x_i in X]
+    ax.plot(x, y, z)
+    plt.show()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.axis('equal')
+    ax.plot(x, y)
+    plt.show()
+
+
+    #mayble funny, well no but just a bit : b = 8, q = 1, p_theta = 1, p_r = 0, r = 10, a = 1, t = 1125
---
+++ main.py
@@ -0,0 +1,331 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFilter
+import tkinter as tk
+from PIL import ImageTk
+import numpy as np
+import csv
+import time
+import random
+import compute
+
+
+def cut_3(n):
+    if n % 3 == 0:
+        x = n/3
+        return [x, x, x]
+    elif n % 3 == 1:
+        x = (n - 1) / 3
+        return [x, x + 1, x]
+    else:
+        x = (n - 2) / 3
+        return [x + 1, x, x + 1]
+
+
+def cutting(v, with_center=False):
+    #x = [[a, b], [c, d]]
+    len = [v[i][1] - v[i][0] for i in range(2)]
+    deltas = [cut_3(len_i) for len_i in len]
+    new = [[v[0][0]], [v[1][0]]]
+    for i in range(3):
+        new[0].append(new[0][-1] + deltas[0][i])
+        new[1].append(new[1][-1] + deltas[1][i])
+    X = [[[new[0][i], new[0][i+1]], [new[1][j], new[1][j+1]], True] for i in range(3) for j in range(3)]
+    if not with_center:
+        X[4][2] = False
+    return X
+
+
+def find_divisors(n, d, k):
+    """
+    returns the greatest k such that d^k divides n
+    :param n: the number that we want to divide
+    :param d: the divisor
+    :param k: the greatest number such that d^k | n
+    :return: k
+    """
+    if n % d == 0:
+        return find_divisors(n / d, d, k + 1)
+    else:
+        return k
+
+
+#window = tk.Tk()
+#window.title("Hello there")
+
+Im = Image.open("Images/0.png")
+pixels = Im.load()
+pixels_copy = Image.open("Images/1.png").load()
+
+#img = tk.PhotoImage(file='Images/24.png')
+#img = ImageTk.PhotoImage(Im)
+#label = tk.Label(window, image=img)
+#label.pack()
+
+X = [[[0, Im.size[0]-1], [0, Im.size[1]-1], True]]
+
+"""def update_image():
+    global X
+    im = ImageTk.PhotoImage(Im)
+    label.configure(image=im)
+    label.image = im
+    label.after(20, update_image)
+    #ImageDraw.Draw(Im).rectangle(X[0], fill=pixels_copy[int((X[0][0][0] + X[0][0][1])/2), int((X[0][1][0] + X[0][1][1])/2)])
+    for i in range(12):
+        if X[0][2]:
+            #ImageDraw.Draw(Im).rectangle([(X[0][0][0], X[0][1][0]), (X[0][0][1], X[0][1][1])], fill=(int(255 * random.random()), int(255 * random.random()), int(255 * random.random())))
+            ImageDraw.Draw(Im).rectangle([(X[0][0][0], X[0][1][0]), (X[0][0][1]-1, X[0][1][1]-1)], fill=pixels_copy[int((X[0][0][0] + X[0][0][1])/2), int((X[0][1][0] + X[0][1][1])/2)])
+        X_add = cutting(X[0])
+        del X[0]
+        X = X + X_add"""
+
+"""
+def update_image():
+    global X
+    im = ImageTk.PhotoImage(Im)
+    label.configure(image=im)
+    label.image = im
+    print(time.process_time())
+    label.after(1000, update_image)
+    print(time.process_time())
+    print("--------------------")
+    ImageDraw.Draw(Im).rectangle([(500, 500), (1000, 1000)], fill=(int(100*(1+np.sin(time.process_time()))), int(105*(1+np.sin(2 * time.process_time()))), int(100*(1+np.sin(1.5 * time.process_time())))))
+"""
+
+
+class ImagePerso:
+    def __init__(self, window, image_path, system=None):
+        self.window = window
+        self.im = Image.open(image_path)
+        #self.imtk = ImageTk.PhotoImage(self.im)
+        self.pixels_copy = Image.open(image_path).load()
+        #self.label = tk.Label(self.window, image=self.imtk)
+        #self.label.pack()
+        self.size = self.im.size
+        self.i = [int(self.size[i]) for i in range(2)]
+        self.Mod = [5 * np.copy(self.size)[i] for i in range(2)]
+        self.dx = np.copy(self.size)[0]
+        self.dy = np.copy(self.size)[1]
+        self.kx = 1
+        self.ky = 1
+        self.qx = 1
+        self.qy = 1
+        self.qmin = min(self.qx, self.qy)
+        self.i = 0
+        self.j = 0
+        self.k = 0
+        self.system = system
+
+    def next_rectangle(self):
+        if self.Mod[0] == 1 and self.Mod[1] == 1:
+            return 0
+        elif self.i == self.qx - 1 and self.j == self.qy - 1:
+            if self.Mod[0] % 5 == 0:
+                self.Mod[0] = self.Mod[0] / 5
+            elif self.Mod[0] % 3 == 0:
+                self.Mod[0] = self.Mod[0] / 3
+            elif self.Mod[0] % 2 == 0:
+                self.Mod[0] = self.Mod[0] / 2
+
+            if self.Mod[1] % 5 == 0:
+                self.Mod[1] = self.Mod[1] / 5
+            elif self.Mod[1] % 3 == 0:
+                self.Mod[1] = self.Mod[1] / 3
+            elif self.Mod[1] % 2 == 0:
+                self.Mod[1] = self.Mod[1] / 2
+
+            self.next_qs()
+        else:
+            self.next_position()
+
+        #if not ((self.i + np.ceil(self.dx / 2) - 1) % self.Mod[0] == 0 and (self.j + np.ceil(self.dy / 2) - 1) % self.Mod[1] == 0):
+        #if not (np.ceil((self.i + 1 / 2) * self.dx) % np.ceil(self.Mod[0] / 2) == 0 and np.ceil((self.j + 1 / 2) * self.dy) % np.ceil(self.Mod[1] / 2) == 0):
+        #if not ((self.i * self.dx) % self.Mod[0] == np.ceil(self.dx / 2) - 1 and (self.j * self.dy) % self.Mod[1] == np.ceil(self.dy / 2) - 1):
+        """if not (self.i % self.kx == np.ceil(self.kx / 2) - 1 and self.j % self.ky == np.ceil(self.ky / 2) - 1) or self.Mod[0] == self.size[0]:
+            #print(np.ceil((self.i + 1 / 2) * self.dx), np.ceil(self.Mod[0] / 2))
+            #print("well, there should be something", self.i, self.j)
+            rectangle_position = [(self.i * self.dx, self.j * self.dy), ((self.i + 1) * self.dx - 1, (self.j + 1) * self.dy - 1)]
+            #color = self.pixels_copy[(self.i + np.ceil(self.dx / 2) - 1) * self.qx, (self.j + np.ceil(self.dy / 2) - 1) * self.qy]
+            color = self.pixels_copy[np.ceil((self.i + 1 / 2) * self.dx) - 1, np.ceil((self.j + 1 / 2) * self.dy) - 1]
+            #color = (int(255 * random.random()), int(255 * random.random()), int(255 * random.random()))
+            #ImageDraw.Draw(self.im).rectangle(rectangle_position, color)
+            return rectangle_position, color"""
+        if not (self.i % self.kx == 0 and self.j % self.ky == 0) or self.Mod[0] == self.size[0] or self.kx == 1 == self.ky:
+            rectangle_position = [(self.i * self.dx, self.j * self.dy), ((self.i + 1) * self.dx - 1, (self.j + 1) * self.dy - 1)]
+            if self.system is None:
+                color = self.pixels_copy[self.i * self.dx, self.j * self.dy]
+            else:
+                color = self.system.compute_angle(self.i * self.dx, self.j * self.dy)
+            return rectangle_position, color
+        else:
+            #print("Nothing happened", self.i, self.j)
+            return self.next_rectangle()
+
+    def next_qs(self):
+        print("next", self.Mod)
+        self.kx = 1
+        if self.Mod[0] % 5 == 0:
+            self.dx = self.dx / 5
+            self.qx = 5 * self.qx
+            self.kx = 5
+        elif self.Mod[0] % 3 == 0:
+            self.dx = self.dx / 3
+            self.qx = 3 * self.qx
+            self.kx = 3
+        elif self.Mod[0] % 2 == 0:
+            self.dx = self.dx / 2
+            self.qx = 2 * self.qx
+            self.kx = 2
+        self.i = 0
+
+        self.ky = 1
+        if self.Mod[1] % 5 == 0:
+            self.dy = self.dy / 5
+            self.qy = 5 * self.qy
+            self.ky = 5
+        elif self.Mod[1] % 3 == 0:
+            self.dy = self.dy / 3
+            self.qy = 3 * self.qy
+            self.ky = 3
+        elif self.Mod[1] % 2 == 0:
+            self.dy = self.dy / 2
+            self.qy = 2 * self.qy
+            self.ky = 2
+        self.j = 0
+
+        self.k = 0
+
+        self.qmin = min(self.qx, self.qy)
+
+        print(self.qx, self.qy)
+
+    def next_position(self):
+        if self.k < self.qmin - 1:
+            if self.j == 0:
+                self.k += 1
+                self.i = 0
+                self.j = self.k
+            else:
+                self.j -= 1
+                self.i += 1
+        #elif self.k <= self.qmin - 1 and self.qy <= self.qx:
+        elif self.k < self.qx + self.qy - 1 - self.qmin and self.qy <= self.qx:
+            if self.j == 0:
+                self.k += 1
+                #self.i = self.k - self.qmin + 1
+                self.j = self.qy - 1
+                self.i = self.k - self.j
+            else:
+                self.j -= 1
+                self.i += 1
+        #elif self.k == self.qmin - 1:
+        elif self.k < self.qx + self.qy - 1 - self.qmin:
+            if self.i == self.qx - 1:
+                self.k += 1
+                self.i = 0
+                #self.j = self.k - self.qy + 1
+                self.j = self.k
+            else:
+                self.j -= 1
+                self.i += 1
+        elif self.qx + self.qy - 1 - self.qmin <= self.k:
+            if self.i == self.qx - 1:
+                self.k += 1
+                self.j = self.qy - 1
+                self.i = self.k - self.j
+            else:
+                self.j -= 1
+                self.i += 1
+
+"""
+def update_image():
+    global X
+    im = ImageTk.PhotoImage(Im)
+    label.configure(image=im)
+    label.image = im
+    label.after(20, update_image)
+    #ImageDraw.Draw(Im).rectangle(X[0], fill=pixels_copy[int((X[0][0][0] + X[0][0][1])/2), int((X[0][1][0] + X[0][1][1])/2)])
+    for i in range(12):
+        if X[0][2]:
+            #ImageDraw.Draw(Im).rectangle([(X[0][0][0], X[0][1][0]), (X[0][0][1], X[0][1][1])], fill=(int(255 * random.random()), int(255 * random.random()), int(255 * random.random())))
+            ImageDraw.Draw(Im).rectangle([(X[0][0][0], X[0][1][0]), (X[0][0][1]-1, X[0][1][1]-1)], fill=pixels_copy[int((X[0][0][0] + X[0][0][1])/2), int((X[0][1][0] + X[0][1][1])/2)])
+        X_add = cutting(X[0])
+        del X[0]
+        X = X + X_add"""
+
+"""
+window = tk.Tk()
+window.title("Hello there")
+im = Image.open("Images/0.png")
+imtk = ImageTk.PhotoImage(im)
+label = tk.Label(window, image=imtk)
+label.pack()
+imp = ImagePerso(window, "Images/0.png")
+"""
+
+"""def update_image_2():
+    imp.label.configure(image=imp.imtk)
+    imp.label.image = imp.imtk
+    imp.label.after(1000, update_image_2)
+    for i in range(1):
+        rectangle_position, color = imp.next_rectangle()
+        ImageDraw.Draw(imp.im).rectangle(rectangle_position, color)"""
+"""
+def update_image_2():
+    global imtk
+    imtk = ImageTk.PhotoImage(im)
+    label.configure(image=imtk)
+    label.image = imtk
+    label.after(10, update_image_2)
+    for i in range(100):
+        rectangle_position, color = imp.next_rectangle()
+        ImageDraw.Draw(im).rectangle(rectangle_position, color)"""
+
+syst = compute.System(0.9, [30, np.pi / 2, 0], [np.pi / 3, np.pi / 3], [720, 720], "Images/Hello_there.png", "Images/eso0932a.tif")
+syst.create_image("Images/Hello_there.png")
+
+window = tk.Tk()
+window.title("Hello there")
+im = Image.open("Images/Hello_there.png")
+imtk = ImageTk.PhotoImage(im)
+label = tk.Label(window, image=imtk)
+label.pack()
+imp = ImagePerso(window, "Images/Hello_there.png", syst)
+
+
+def update_image_3():
+    global imtk
+    imtk = ImageTk.PhotoImage(im)
+    label.configure(image=imtk)
+    label.image = imtk
+    label.after(100, update_image_3)
+    for i in range(6):
+        rectangle_position, color = imp.next_rectangle()
+        ImageDraw.Draw(im).rectangle(rectangle_position, color)
+
+
+window.after(100, update_image_3)
+window.mainloop()
+
+"""
+pixels = im.load()
+for i in range(im.size[0]):
+    for j in range(im.size[1]):
+        pixels[i, j] = syst.compute_angle(i, j)
+        if j == 0:
+            print(i / im.size[0])
+im.show()"""
+
+"""
+def update_image():
+    global tkimg1
+    tkimg1 = ImageTk.PhotoImage(Image.open('Images/0.png'))
+    label.config(image=tkimg1)
+    label.after(1000, update_image)
+w = tk.Tk()
+im = Image.open('Images/24.png')
+tkimg1 = ImageTk.PhotoImage(im)
+label = tk.Label(w, image=tkimg1)
+label.pack()
+w.after(1000, update_image)
+w.mainloop()"""
